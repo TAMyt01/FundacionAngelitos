90405c7fd8d62cd0d4f689172f55a39c7a7c0554
TAMyt01 <albaricoqueruso@gmail.com>
2026-04-14 02:01:16 -0600

    actions

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import { resolve } from "node:path";
-
-import HandlebarsPlugin from 'vite-plugin-handlebars';
-
+import HandlebarsPlugin from "vite-plugin-handlebars";
 import getData from "./data";
 
 export default defineConfig({
-    plugins: [
-        HandlebarsPlugin({
-            partialDirectory: resolve(__dirname, 'partials'),
-            context: (page) => {
-                return getData(page);
-            }
-        })
-    ]
+  base: process.env.DEPLOY_BASE_URL || "/",
+  plugins: [
+    HandlebarsPlugin({
+      partialDirectory: resolve(__dirname, "partials"),
+      context: (page) => getData(page),
+    }),
+  ],
 });
